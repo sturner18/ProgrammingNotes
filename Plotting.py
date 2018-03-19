df3af98a9c7776ed73d34cc0f3b44528b2762c26
simoneturner <sturner18@fwparker.org>
2018-03-19 10:37:05 -0500

    March 9

---
+++ Plotting.py
@@ -0,0 +1,23 @@
+# PLOTTING (with mathplotlib)
+
+import matplotlib.pyplot as plt
+
+plt.figure(1) # creates a new window
+
+plt.plot([1, 2, 3, 4]) # if there is no x axis, it just gives index 0, 1, 2 ...
+plt.plot([1, 2, 3, 4,], [1, 4, 9, 16])
+
+plt.figure(2, facecolor='pink') # opens a new window/figure
+
+x = [x for x in range(10)]
+y = [y ** 2 for y in range(10)]
+
+# plt.plot takes kwarg (keyword arguments)
+plt.plot(x, y, color='pink', marker='+', markersize=10, linestyle=':', alpha=0.85)
+
+plt.xlabel('time (days)', color='blue')
+plt.ylabel('excitement level (yays)', color='blue')
+plt.title('Example Plot', color='blue', fontsize=30)
+plt.axis([1, 10, 10, 100]) # x min, x max, y min, y max
+
+plt.show()
